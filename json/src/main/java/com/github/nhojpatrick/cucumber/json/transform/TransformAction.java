0d7e140b7f2bcbc0e419dda0b711a6f5911b2c06
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2018-12-08 20:20:37 +0000

    Transform Action

--- json/src/main/java/com/github/nhojpatrick/cucumber/json/transform/TransformAction.java
+++ json/src/main/java/com/github/nhojpatrick/cucumber/json/transform/TransformAction.java
@@ -1,17 +1,27 @@
 package com.github.nhojpatrick.cucumber.json.transform;
 
 import com.github.nhojpatrick.cucumber.json.exceptions.InvalidTransformActionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.List;
 
 public enum TransformAction {
 
-    REMOVE,
+    REMOVE("Remove"),
 
-    SET,
+    SET("Set"),
 
-    WHITESPACE;
+    WHITESPACE("Whitespace");
+
+    private static Logger LOGGER = LoggerFactory.getLogger(TransformAction.class);
+
+    private String value;
+
+    TransformAction(final String value) {
+        this.value = value;
+    }
 
     private static final List<TransformAction> ENUM_CACHE = Arrays.asList(values());
 
@@ -19,7 +29,17 @@ public enum TransformAction {
             throws InvalidTransformActionException {
 
         return ENUM_CACHE.stream()
-                .filter(p -> p.name().equalsIgnoreCase(action))
+                // FIXME name().equalsIgnoreCase deprecated in future release
+                .map(p -> {
+                    if (!p.value.equals(action)
+                            && p.name().equalsIgnoreCase(action)) {
+                        LOGGER.warn(String.format("TransformAction.resolve(%s) Case Insensitive matching is deprecated", action));
+                    }
+                    return p;
+                })
+                .filter(p -> p.value.equals(action)
+                        || p.name().equalsIgnoreCase(action)
+                )
                 .findFirst()
                 .orElseThrow(() -> new InvalidTransformActionException(action));
     }
