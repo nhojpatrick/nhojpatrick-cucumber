397591102b77c3fbc80f97e1db49e838e76f484f
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2019-11-19 12:28:48 +0000

    json seperate steps into seperate module

---
+++ json-steps/src/main/java11/module-info.java
@@ -0,0 +1,6 @@
+module com.github.nhojpatrick.cucumber.json.steps {
+    requires com.github.nhojpatrick.cucumber.core;
+    requires com.github.nhojpatrick.cucumber.json;
+    requires com.github.nhojpatrick.cucumber.state;
+    requires com.google.guice;
+}
--- json/src/main/java11/module-info.java
+++ json/src/main/java11/module-info.java
@@ -1,4 +1,10 @@
 module com.github.nhojpatrick.cucumber.json {
+    exports com.github.nhojpatrick.cucumber.json.exceptions;
+    exports com.github.nhojpatrick.cucumber.json.map;
+    exports com.github.nhojpatrick.cucumber.json.transform;
+    exports com.github.nhojpatrick.cucumber.json.transform.transformations;
+    exports com.github.nhojpatrick.cucumber.json.transform.utils;
+    exports com.github.nhojpatrick.cucumber.json.validation;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
     requires com.github.nhojpatrick.cucumber.core;
