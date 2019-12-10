f47a5012e09e9727a1359eb0d714a48f7d695d40
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2019-12-10 18:29:44 +0000

    SpotBugs fix violations

--- json-testing-objects/src/main/java11/module-info.java
+++ json-testing-objects/src/main/java11/module-info.java
@@ -2,6 +2,8 @@ module com.github.nhojpatrick.cucumber.json.testing.objects {
     exports com.github.nhojpatrick.cucumber.json.testing.objects;
     opens com.github.nhojpatrick.cucumber.json.testing.objects
             to com.fasterxml.jackson.databind;
+    requires com.github.spotbugs.annotations;
+    requires org.joda.beans;
     requires org.slf4j;
     requires transitive com.fasterxml.jackson.databind;
 }
