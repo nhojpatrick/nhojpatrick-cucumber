13e205c9d5370d20a708c66b442c800fa50ce875
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2019-12-13 18:37:56 +0000

    json map into seperate module

--- json-map-steps/src/main/java11/module-info.java
+++ json-map-steps/src/main/java11/module-info.java
@@ -1,6 +1,6 @@
 module com.github.nhojpatrick.cucumber.json.map.steps {
     exports com.github.nhojpatrick.cucumber.json.map.steps;
-    requires transitive com.github.nhojpatrick.cucumber.json;
+    requires transitive com.github.nhojpatrick.cucumber.json.map;
     requires transitive com.github.nhojpatrick.cucumber.state;
     requires org.slf4j;
 }
