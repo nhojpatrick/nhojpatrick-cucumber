9364a2c93f130c707762d5a584f8c798f4c0e882
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2019-12-14 09:22:41 +0000

    Map Keys into Constant

---
+++ json-map/src/main/java/com/github/nhojpatrick/cucumber/json/map/ConvertToMapConstants.java
@@ -0,0 +1,8 @@
+package com.github.nhojpatrick.cucumber.json.map;
+
+public interface ConvertToMapConstants {
+
+    String DEFAULT_OBJECT_KEY = "runState.json.obj";
+    String DEFAULT_MAP_KEY = "runState.json.map";
+
+}
