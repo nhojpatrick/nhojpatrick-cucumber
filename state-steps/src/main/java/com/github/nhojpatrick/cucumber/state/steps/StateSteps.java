b069ef596ce1d0e5222552d8adce3beffad42235
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2020-01-04 11:00:18 +0000

    StateSteps add format options of \n and \t

--- state-steps/src/main/java/com/github/nhojpatrick/cucumber/state/steps/StateSteps.java
+++ state-steps/src/main/java/com/github/nhojpatrick/cucumber/state/steps/StateSteps.java
@@ -47,9 +47,12 @@ public class StateSteps {
         for (final Map.Entry<String, String> row : stringStringMap.entrySet()) {
             final Object runStateValueObj = this.runState.get(row.getKey(), Object.class);
             final String runStateValueAsStr = String.valueOf(runStateValueObj);
+            final String rowValue = row.getValue()
+                    .replace("\\n", "\n")
+                    .replace("\\t", "\t");
             assertThat(String.format("Unexpected RunState for key '%s'", row.getKey()),
                     runStateValueAsStr,
-                    is(equalTo(row.getValue()))
+                    is(equalTo(rowValue))
             );
         }
     }
