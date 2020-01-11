a66d8e9809291cf16c2c08e3793e11607c5d95d4
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2020-01-11 14:22:53 +0000

    spotbugs

--- json-jodabeans/src/main/java11/module-info.java
+++ json-jodabeans/src/main/java11/module-info.java
@@ -1,5 +1,6 @@
 module com.github.nhojpatrick.cucumber.json.jodabeans {
     exports com.github.nhojpatrick.cucumber.json.jodabeans.transform;
+    requires com.github.spotbugs.annotations;
     requires org.apache.commons.lang3;
     requires org.slf4j;
     requires transitive org.joda.beans;
