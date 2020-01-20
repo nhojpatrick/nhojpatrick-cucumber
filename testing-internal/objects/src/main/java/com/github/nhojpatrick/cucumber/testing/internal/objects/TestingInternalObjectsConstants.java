723084728b1940c3129c70bee2eefc584f59c40a
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2020-01-20 08:27:12 +0000

    new basic arrays

--- testing-internal/objects/src/main/java/com/github/nhojpatrick/cucumber/testing/internal/objects/TestingInternalObjectsConstants.java
+++ testing-internal/objects/src/main/java/com/github/nhojpatrick/cucumber/testing/internal/objects/TestingInternalObjectsConstants.java
@@ -1,4 +1,79 @@
 package com.github.nhojpatrick.cucumber.testing.internal.objects;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TestingInternalObjectsConstants {
+
+    public static final Map<String, Object> MAP_BASIC_ARRAYS;
+
+    public static final BasicArraysOuterObj OBJECT_BASIC_ARRAYS;
+
+    public static Map<String, Object> getMapBasicArrays() {
+
+        final List<Map<String, Object>> objectsArray = new ArrayList<>();
+
+        final Map<String, Object> aObjectArray = new HashMap<>();
+        aObjectArray.put(BasicArraysInnerObj.OBJECT_ARRAY_ID, "aObjectArrayId");
+        objectsArray.add(aObjectArray);
+
+        final Map<String, Object> bObjectArray = new HashMap<>();
+        bObjectArray.put(BasicArraysInnerObj.OBJECT_ARRAY_ID, "bObjectArrayId");
+        objectsArray.add(bObjectArray);
+
+        final Map<String, Object> cObjectArray = new HashMap<>();
+        cObjectArray.put(BasicArraysInnerObj.OBJECT_ARRAY_ID, "cObjectArrayId");
+        objectsArray.add(cObjectArray);
+
+        final Map<String, Object> dObjectArray = new HashMap<>();
+        dObjectArray.put(BasicArraysInnerObj.OBJECT_ARRAY_ID, "dObjectArrayId");
+        objectsArray.add(dObjectArray);
+
+        final List<String> primitivesArray = new ArrayList<>();
+        primitivesArray.add("aPrimitiveArray");
+        primitivesArray.add("bPrimitiveArray");
+        primitivesArray.add("cPrimitiveArray");
+        primitivesArray.add("dPrimitiveArray");
+
+        final Map<String, Object> basicArrays = new LinkedHashMap<>();
+
+        basicArrays.put(BasicArraysOuterObj.OBJECTS_ARRAY, objectsArray);
+        basicArrays.put(BasicArraysOuterObj.OBJECTS_EMPTY, new ArrayList());
+        basicArrays.put(BasicArraysOuterObj.OBJECTS_NULL, null);
+        basicArrays.put(BasicArraysOuterObj.PRIMITIVES_ARRAY, primitivesArray);
+        basicArrays.put(BasicArraysOuterObj.PRIMITIVES_EMPTY, new ArrayList());
+        basicArrays.put(BasicArraysOuterObj.PRIMITIVES_NULL, null);
+
+        return basicArrays;
+    }
+
+    static {
+        MAP_BASIC_ARRAYS = Collections.unmodifiableMap(new LinkedHashMap(getMapBasicArrays()));
+    }
+
+    public static BasicArraysOuterObj getObjectBasicArrays() {
+
+        final List<BasicArraysInnerObj> objectsArray = new ArrayList<>();
+        objectsArray.add(new BasicArraysInnerObj("aObjectArrayId"));
+        objectsArray.add(new BasicArraysInnerObj("bObjectArrayId"));
+        objectsArray.add(new BasicArraysInnerObj("cObjectArrayId"));
+        objectsArray.add(new BasicArraysInnerObj("dObjectArrayId"));
+
+        final List<String> primitivesArray = new ArrayList<>();
+        primitivesArray.add("aPrimitiveArray");
+        primitivesArray.add("bPrimitiveArray");
+        primitivesArray.add("cPrimitiveArray");
+        primitivesArray.add("dPrimitiveArray");
+
+        return new BasicArraysOuterObj(objectsArray, primitivesArray);
+    }
+
+    static {
+        OBJECT_BASIC_ARRAYS = getObjectBasicArrays();
+    }
+
 }
--- testing-internal/objects/src/test/java/com/github/nhojpatrick/cucumber/testing/internal/objects/TestingInternalObjectsConstantsTest.java
+++ testing-internal/objects/src/test/java/com/github/nhojpatrick/cucumber/testing/internal/objects/TestingInternalObjectsConstantsTest.java
@@ -1,4 +1,62 @@
 package com.github.nhojpatrick.cucumber.testing.internal.objects;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import static com.github.nhojpatrick.cucumber.testing.internal.objects.TestingInternalObjectsConstants.MAP_BASIC_ARRAYS;
+import static com.github.nhojpatrick.cucumber.testing.internal.objects.TestingInternalObjectsConstants.OBJECT_BASIC_ARRAYS;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 public class TestingInternalObjectsConstantsTest {
+
+    @Test
+    public void basicArrays()
+            throws JsonProcessingException {
+
+        final String mapBasicArraysAsStr = new ObjectMapper()
+                .writeValueAsString(MAP_BASIC_ARRAYS);
+
+        final String objectBasicArraysAsStr = new ObjectMapper()
+                .writeValueAsString(OBJECT_BASIC_ARRAYS);
+
+        final String expectation = new StringBuilder()
+                .append("{")
+                .append("\"")
+                .append(BasicArraysOuterObj.OBJECTS_ARRAY)
+                .append("\":[")
+                .append("{\"object_array_id\":\"aObjectArrayId\"},")
+                .append("{\"object_array_id\":\"bObjectArrayId\"},")
+                .append("{\"object_array_id\":\"cObjectArrayId\"},")
+                .append("{\"object_array_id\":\"dObjectArrayId\"}")
+                .append("],\"")
+                .append(BasicArraysOuterObj.OBJECTS_EMPTY)
+                .append("\":[],\"")
+                .append(BasicArraysOuterObj.OBJECTS_NULL)
+                .append("\":null,\"")
+                .append(BasicArraysOuterObj.PRIMITIVES_ARRAY)
+                .append("\":[")
+                .append("\"aPrimitiveArray\",")
+                .append("\"bPrimitiveArray\",")
+                .append("\"cPrimitiveArray\",")
+                .append("\"dPrimitiveArray\"")
+                .append("],\"")
+                .append(BasicArraysOuterObj.PRIMITIVES_EMPTY)
+                .append("\":[],\"")
+                .append(BasicArraysOuterObj.PRIMITIVES_NULL)
+                .append("\":null")
+                .append("}")
+                .toString();
+
+        assertAll("Check Both Ways",
+                () -> assertThat("Maps against Object", mapBasicArraysAsStr, is(equalTo(objectBasicArraysAsStr))),
+                () -> assertThat("Maps against Expectation", mapBasicArraysAsStr, is(equalTo(expectation))),
+                () -> assertThat("Object against Map", objectBasicArraysAsStr, is(equalTo(mapBasicArraysAsStr))),
+                () -> assertThat("Object against Expectation", objectBasicArraysAsStr, is(equalTo(expectation)))
+        );
+    }
+
 }
