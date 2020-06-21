48ff8a2c96c0b1ff95ec62cab6c10300f7953b49
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2020-06-21 10:24:55 +0100

    Spotbugs Ignore FII_USE_FUNCTION_IDENTITY

--- json-transform/src/main/java11/module-info.java
+++ json-transform/src/main/java11/module-info.java
@@ -4,6 +4,7 @@ module com.github.nhojpatrick.cucumber.json.transform {
     exports com.github.nhojpatrick.cucumber.json.transform.factory;
     exports com.github.nhojpatrick.cucumber.json.transform.transformations;
     uses com.github.nhojpatrick.cucumber.json.core.transform.TransformationService;
+    requires com.github.spotbugs.annotations;
     requires org.apache.commons.text;
     requires org.hamcrest;
     requires org.junit.jupiter.api;
