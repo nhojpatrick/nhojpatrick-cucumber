f4981ca4f7e8e1331a9c5722640c5cd65439044c
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2021-08-12 22:04:13 +0100

    JDK Upgrade fixes

--- testing-internal/objects/src/main/java11/module-info.java
+++ testing-internal/objects/src/main/java11/module-info.java
@@ -2,6 +2,7 @@ module com.github.nhojpatrick.cucumber.testing.internal.objects {
     exports com.github.nhojpatrick.cucumber.testing.internal.objects;
     opens com.github.nhojpatrick.cucumber.testing.internal.objects
             to com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
     requires com.github.spotbugs.annotations;
     requires org.joda.beans;
     requires org.slf4j;
