e01d6132ad202c1582782894664f150afe32cfa6
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2021-10-01 08:01:51 +0100

    Cucumber Http Response Glue

--- all-glue/src/main/java11/module-info.java
+++ all-glue/src/main/java11/module-info.java
@@ -3,6 +3,7 @@ module com.github.nhojpatrick.cucumber.all.glue {
     requires org.junit.jupiter.api;
     requires org.slf4j;
     requires transitive com.github.nhojpatrick.cucumber.http.request.glue;
+    requires transitive com.github.nhojpatrick.cucumber.http.response.glue;
     requires transitive com.github.nhojpatrick.cucumber.json.map.glue;
     requires transitive com.github.nhojpatrick.cucumber.json.transform.glue;
     requires transitive com.github.nhojpatrick.cucumber.state.glue;
