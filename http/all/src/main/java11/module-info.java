b4fc6637d28ca591ad8bcd9c7db9c8d88b3e4dc9
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2021-10-01 08:01:51 +0100

    Cucumber Http Response

--- http/all/src/main/java11/module-info.java
+++ http/all/src/main/java11/module-info.java
@@ -2,4 +2,5 @@ module com.github.nhojpatrick.cucumber.http.all {
     requires transitive com.github.nhojpatrick.cucumber.http.constants;
     requires transitive com.github.nhojpatrick.cucumber.http.core;
     requires transitive com.github.nhojpatrick.cucumber.http.request;
+    requires transitive com.github.nhojpatrick.cucumber.http.response;
 }
