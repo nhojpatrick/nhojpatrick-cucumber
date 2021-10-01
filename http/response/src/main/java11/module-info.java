dabcec0b3bb0faeca611e6cf08fa95a6cc29395e
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2021-10-01 08:01:51 +0100

    Cucumber Http Response

---
+++ http/response/src/main/java11/module-info.java
@@ -0,0 +1,6 @@
+module com.github.nhojpatrick.cucumber.http.response {
+    requires transitive com.github.nhojpatrick.cucumber.http.constants;
+    requires transitive com.github.nhojpatrick.cucumber.http.core;
+    requires transitive com.github.nhojpatrick.cucumber.state;
+    requires org.slf4j;
+}
