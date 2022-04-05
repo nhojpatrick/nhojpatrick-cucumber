e13e1931bb8dd5a7d042325d4e4f5d041616035e
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-04-05 01:02:48 +0100

    JPMS fix for Java 17

--- json-map-steps/src/test/java11/module-info.java
+++ json-map-steps/src/test/java11/module-info.java
@@ -2,6 +2,7 @@ open module com.github.nhojpatrick.cucumber.json.map.steps.test {
     requires com.github.nhojpatrick.cucumber.json.map.steps;
     requires org.apache.logging.log4j;
     requires org.hamcrest;
+    requires transitive io.cucumber.junit.platform.engine;
     requires transitive org.junit.jupiter.api;
     requires transitive org.junit.jupiter.engine;
     requires transitive org.junit.platform.suite.api;
