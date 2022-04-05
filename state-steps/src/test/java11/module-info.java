b1da351b472afcca5c249c18e0ca494d1c9ac9e0
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-04-05 01:02:48 +0100

    JPMS fix for Java 17

--- state-steps/src/test/java11/module-info.java
+++ state-steps/src/test/java11/module-info.java
@@ -4,6 +4,7 @@ open module com.github.nhojpatrick.cucumber.state.steps.test {
     requires org.apache.logging.log4j;
     requires org.hamcrest;
     requires org.junit.platform.commons;
+    requires transitive io.cucumber.junit.platform.engine;
     requires transitive org.junit.jupiter.api;
     requires transitive org.junit.jupiter.engine;
     requires transitive org.junit.platform.suite.api;
