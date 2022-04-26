7840f63e9f0057e86653dd2ac77baa6069aa3661
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-04-26 08:51:54 +0100

    cucumber-json-transform module

--- json/all/src/main/java11/module-info.java
+++ json/all/src/main/java11/module-info.java
@@ -1,3 +1,4 @@
 module com.github.nhojpatrick.cucumber.json.all {
     requires transitive com.github.nhojpatrick.cucumber.json.map;
+    requires transitive com.github.nhojpatrick.cucumber.json.transform;
 }
